bab13595f35a3caa7feb6ec8247d997d4c4a531a
anmolsingh3600 <35175849+anmolsingh3600@users.noreply.github.com>
2018-01-17 09:52:03 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,11 @@
+
+public class simple {
+public static void main(String[] args)
+{
+	int a=100;
+	int b=200;
+	
+	System.out.println("add of a and b is"+(a+b));
+	System.out.println("This is addition of two numbers");
+}
+}
