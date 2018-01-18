1f32c6a27f481466c78ddd572278e2d20e92c8a1
anmolsingh3600 <35175849+anmolsingh3600@users.noreply.github.com>
2018-01-18 09:39:23 +0530

    Add files via upload

---
+++ object.java
@@ -0,0 +1,27 @@
+class object{
+	String chairname;
+	int height;
+	int weight;
+	int width;
+	String color;
+	String companyname;
+	
+
+public static void main(String[] args)
+{
+	object a1=new object();
+	
+      a1.chairname="Zbc";
+      a1.height=5;
+      a1.weight=7;
+      a1.width=8;
+      a1.color="brown";
+      a1.companyname="axc";
+      System.out.println("The chair name is "+a1.chairname +" its height is "+a1.height+" its weight is "+a1.weight+" its width is "+a1.width+" its color is "+a1.color+" its company name is "+a1.companyname );
+      }
+
+
+
+
+
+}
