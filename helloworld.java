26b84925a1973a8c0e91e9f09e07e018d458ab22
anmolsingh3600 <35175849+anmolsingh3600@users.noreply.github.com>
2018-01-25 09:12:28 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,47 @@
+
+public class helloworld {
+public static void main(String[] args) {
+		int a=100;
+		int b=12;
+		int c=14;
+		int d=a*b+b*c;
+System.out.println("This is java...........");
+		System.out.println(d);
+	
+		byte age=50;
+	short s=3333;
+	int i=100000;
+	long l=100000000;
+	
+	System.out.println("----------------------------------");
+	System.out.println("age is" +age);
+	System.out.println("s is"+s);
+	System.out.println("i is"+i);
+	System.out.println("l is"+l);
+	
+	System.out.println("----------------------------------");
+	float f = 2.2f;
+	double z = 2.2;
+	
+	System.out.println("f is: "+f);
+	System.out.println("z is: "+z);
+	
+	System.out.println("----------------------------------");
+	char ch = 'A';
+
+	ch = '\u0907';
+	System.out.println("ch is: "+ch);
+	
+	System.out.println("----------------------------------");
+	
+	boolean flag = true;
+	flag = false;
+	
+	System.out.println("----------------------------------");
+	
+	int n5 = 8;
+	int n6 = 10;
+	int n7 = n5 & n6;
+	System.out.println("n7 is: "+n7);
+}
+}
